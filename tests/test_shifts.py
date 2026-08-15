4bccbdea50aa0aa58e7cf3bdc32c7965ea9b82e9
HelioGuilherme66 <helioxentric@gmail.com>
2026-08-15 23:56:07 +0100

    Add Fields to ShiftCreate

--- tests/test_shifts.py
+++ tests/test_shifts.py
@@ -333,4 +333,3 @@ def test_create_shift_recurring_by_period(client: TestClient, worker_id: int):
         print(f"DEBUG: test_create_shift_recurring_by_period end_time={body['end_time']}"
               f" computed={final_end_time}")
     assert body["end_time"] == final_end_time
-
