e80f03413f587b3741e5469d93ac4718475bdbc7
HelioGuilherme66 <helioxentric@gmail.com>
2026-08-21 04:14:49 +0100

    Cleanup spaces after rebase

--- app/routers/shifts.py
+++ app/routers/shifts.py
@@ -342,4 +342,3 @@ def delete_shift(request: Request, shift_id: int, session: Session = Depends(get
         raise HTTPException(status_code=404, detail="Shift not found")
     session.delete(shift)
     session.commit()
-
